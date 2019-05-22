137fdf95ef9058be9aad45ac69606b6dd0a46fc4
woodha <woodha@Serhiiwood.local>
2019-05-23 02:36:20 +0300

    add markup and styles

---
+++ script/index.js
@@ -0,0 +1,14 @@
+// AOS Settings
+
+window.onload = function() {
+	AOS.init({
+		duration: 2000,
+	});
+}
+
+// TASK CARDS TOGGLER
+
+$('.task__solution').hide();
+$( '.task__solution-btn').click(function() {
+	$(this).next().toggle('slow');
+});
